d516f7d0b6fe8eb7a9e62f0a99a35c8ffe3a0e62
nggit <12218311+nggit@users.noreply.github.com>
2023-10-21 21:22:33 +0700

    optimize getoptions

--- tremolo/tremolo.py
+++ tremolo/tremolo.py
@@ -163,12 +163,14 @@ class Tremolo:
 
     def getoptions(self, func):
         options = {}
+        arg_count = func.__code__.co_argcount
 
         if func.__defaults__ is not None:
-            options = dict(zip(
-                func.__code__.co_varnames[:len(func.__defaults__)],
-                func.__defaults__
-            ))
+            arg_count -= len(func.__defaults__)
+
+        for i, name in enumerate(func.__code__.co_varnames[
+                                     arg_count:func.__code__.co_argcount]):
+            options[name] = func.__defaults__[i]
 
         return options
 
