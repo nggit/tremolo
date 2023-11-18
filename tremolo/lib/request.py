791ed65877a98ec88dc28325a2b2396e4551c31f
nggit <12218311+nggit@users.noreply.github.com>
2023-11-19 04:31:00 +0700

    clean up

--- tremolo/lib/request.py
+++ tremolo/lib/request.py
@@ -4,46 +4,41 @@ import asyncio
 
 
 class Request:
-    __slots__ = ('_protocol', 'body_size')
+    __slots__ = ('protocol', 'body_size')
 
     def __init__(self, protocol):
-        self._protocol = protocol
-
+        self.protocol = protocol
         self.body_size = 0
 
     @property
-    def protocol(self):
-        return self._protocol
-
-    @property
     def context(self):
-        return self._protocol.context
+        return self.protocol.context
 
     @property
     def ctx(self):
-        return self._protocol.context
+        return self.protocol.context
 
     @property
     def transport(self):
-        return self._protocol.transport
+        return self.protocol.transport
 
     def clear_body(self):
         self.body_size = 0
 
     async def recv(self):
-        while self._protocol.queue[0] is not None:
-            task = self._protocol.loop.create_task(
-                self._protocol.queue[0].get()
+        while self.protocol.queue[0] is not None:
+            task = self.protocol.loop.create_task(
+                self.protocol.queue[0].get()
             )
-            timer = self._protocol.loop.call_at(
-                self._protocol.loop.time() +
-                self._protocol.options['keepalive_timeout'],
+            timer = self.protocol.loop.call_at(
+                self.protocol.loop.time() +
+                self.protocol.options['keepalive_timeout'],
                 task.cancel
             )
 
             try:
                 await task
-                self._protocol.queue[0].task_done()
+                self.protocol.queue[0].task_done()
             except asyncio.CancelledError:
                 raise TimeoutError('recv timeout')
             finally:
