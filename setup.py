acfb1906f598b8c6e16cf9fe451c3eae0b18f5f4
nggit <12218311+nggit@users.noreply.github.com>
2023-11-20 06:14:24 +0700

    release 0.0.304

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='tremolo',
-    version='0.0.303',
+    version='0.0.304',
     license='MIT',
     author='nggit',
     author_email='contact@anggit.com',
--- tremolo/__init__.py
+++ tremolo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.303'
+__version__ = '0.0.304'
 
 from .tremolo import Tremolo  # noqa: E402
 from . import exceptions  # noqa: E402,F401
