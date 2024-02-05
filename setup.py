79b80768ca9bc7390f59090c71a73788161dfe4f
nggit <12218311+nggit@users.noreply.github.com>
2024-02-05 17:04:43 +0700

    release 0.0.318

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='tremolo',
-    version='0.0.317',
+    version='0.0.318',
     license='MIT',
     author='nggit',
     author_email='contact@anggit.com',
