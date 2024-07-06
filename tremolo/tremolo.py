893ce2d06928723837689944a7d39f98104d9056
nggit <contact@anggit.com>
2024-07-06 17:46:39 +0700

    refactor pool, add max_connections (#82)
    * refactor pool, add max_connections
    
    * release 0.0.507
    
    ---------
    
    Co-authored-by: nggit <12218311+nggit@users.noreply.github.com>

--- tremolo/tremolo.py
+++ tremolo/tremolo.py
@@ -23,7 +23,7 @@ from .utils import (  # noqa: E402
 from .lib.connections import KeepAliveConnections  # noqa: E402
 from .lib.contexts import ServerContext as WorkerContext  # noqa: E402
 from .lib.locks import ServerLock  # noqa: E402
-from .lib.pools import QueuePool  # noqa: E402
+from .lib.pools import ObjectPool  # noqa: E402
 
 _REUSEPORT_OR_REUSEADDR = {
     True: getattr(socket, 'SO_REUSEPORT', socket.SO_REUSEADDR),
@@ -271,9 +271,7 @@ class Tremolo:
         connections = KeepAliveConnections(
             maxlen=options.get('keepalive_connections', 512)
         )
-        pools = {
-            'queue': QueuePool(1024, self._logger)
-        }
+        pool = ObjectPool(pool_size=options.get('max_connections', 1000))
 
         if 'app' in options and isinstance(options['app'], str):
             from .asgi_lifespan import ASGILifespan
@@ -359,7 +357,7 @@ class Tremolo:
                            ),
                            server_info=server_info,
                            _connections=connections,
-                           _pools=pools,
+                           _pool=pool,
                            _app=options['app'],
                            _app_close_timeout=options.get(
                                'app_close_timeout', 30
