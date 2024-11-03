e6e2131b5f1f89e683dc77d6670571bd44f8003e
nggit <12218311+nggit@users.noreply.github.com>
2024-11-03 13:44:33 +0700

    add request.uid()

--- tremolo/lib/http_response.py
+++ tremolo/lib/http_response.py
@@ -418,19 +418,7 @@ class HTTPResponse(Response):
                     )
                     size -= buffer_size
             else:
-                client = self.request.client
-
-                if client is None:
-                    fileno = self.request.socket.fileno()
-                    client = (str(fileno), fileno)
-
-                ip, port = client
-                boundary = b'----Boundary%x%x%x%x' % (hash(ip),
-                                                      port,
-                                                      os.getpid(),
-                                                      int.from_bytes(
-                                                          os.urandom(4),
-                                                          byteorder='big'))
+                boundary = b'----Boundary%s' % self.request.uid()
 
                 self.set_content_type(
                     b'multipart/byteranges; boundary=%s' % boundary
