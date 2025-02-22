6c282777774ff35ec5f800203f31631a9b6329c4
nggit <contact@anggit.com>
2025-02-22 16:42:33 +0700

    revamp ASGI, add ASGIWrapper

--- tremolo/asgi_server.py
+++ tremolo/asgi_server.py
@@ -21,90 +21,93 @@ _WSS_OR_WS = {
 
 
 class ASGIServer(HTTPProtocol):
-    __slots__ = ('response', '_scope', '_read', '_websocket', '_waiter')
+    __slots__ = ('ON_CLOSE',)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self._scope = {
-            'asgi': {'version': '3.0', 'spec_version': '2.3'},
-            'root_path': self.options['root_path'],
-            'server': self.globals.info['server']
-        }
-        self.response = None  # set in request_received
-        self._read = None
-        self._websocket = None
-        self._waiter = self.loop.create_future()
+        self.ON_CLOSE = self.loop.create_future()
 
-    def _handle_websocket(self):
-        self._websocket = WebSocket(self.request, self.response)
+    def connection_lost(self, exc):
+        super().connection_lost(exc)
 
-        self._scope['type'] = 'websocket'
-        self._scope['scheme'] = _WSS_OR_WS[self.request.scheme == b'https']
-        self._scope['subprotocols'] = [
-            value.decode('latin-1') for value in
-            self.request.headers.getlist(b'sec-websocket-protocol')]
-
-    def _handle_http(self):
-        self._read = self.request.stream()
-
-        self._scope['type'] = 'http'
-        self._scope['method'] = self.request.method.decode('latin-1')
-        self._scope['scheme'] = self.request.scheme.decode('latin-1')
+        if not self.ON_CLOSE.done():
+            self.ON_CLOSE.set_result(None)
 
     async def request_received(self, request, response):
-        self.response = response
-
-        if not self.request.is_valid:
-            await error_400(request=self.request, response=response)
+        if not request.is_valid:
+            await error_400(request=request, response=response)
             return
 
-        self._scope.update(
-            http_version=self.request.version.decode('latin-1'),
-            path=unquote_to_bytes(self.request.path).decode('latin-1'),
-            raw_path=self.request.path,
-            query_string=self.request.query_string,
-            headers=self.request.header.getheaders(),
-            client=self.request.client
-        )
+        scope = {
+            'asgi': {'version': '3.0', 'spec_version': '2.3'},
+            'root_path': self.options['root_path'],
+            'server': self.globals.info['server'],
+            'client': request.client,
+            'http_version': request.version.decode('latin-1'),
+            'path': unquote_to_bytes(request.path).decode('latin-1'),
+            'raw_path': request.path,
+            'query_string': request.query_string,
+            'headers': request.header.getheaders()
+        }
 
-        if (self.options['ws'] and
-                b'sec-websocket-key' in self.request.headers and
-                b'upgrade' in self.request.headers and
-                self.request.headers[b'upgrade'].lower() == b'websocket'):
-            self._handle_websocket()
+        if (self.options['ws'] and b'sec-websocket-key' in request.headers and
+                b'upgrade' in request.headers and
+                request.headers[b'upgrade'].lower() == b'websocket'):
+            scope['type'] = 'websocket'
+            scope['scheme'] = _WSS_OR_WS[request.scheme == b'https']
+            scope['subprotocols'] = [
+                value.decode('latin-1') for value in
+                request.headers.getlist(b'sec-websocket-protocol')]
         else:
-            self._handle_http()
+            scope['type'] = 'http'
+            scope['method'] = request.method.decode('latin-1')
+            scope['scheme'] = request.scheme.decode('latin-1')
 
         try:
-            await self.options['app'](self._scope, self.receive, self.send)
+            await ASGIWrapper(self.options['app'], scope, request, response)
 
-            if not self._waiter.done():
+            if not self.ON_CLOSE.done():
                 self.logger.info('handler exited early (no close?)')
-                await self._waiter
+                await self.ON_CLOSE
         except (asyncio.CancelledError, Exception) as exc:
-            if (self.request is not None and self.request.upgraded and
-                    self._websocket is not None):
+            if scope['type'] == 'websocket' and request.upgraded:
                 exc = WebSocketServerClosed(cause=exc)
 
             await response.handle_exception(exc)
+        finally:
+            scope.clear()
 
     async def error_received(self, exc, response):
         return await error_500(
             request=response.request, response=response, exc=exc
         )
 
-    def connection_lost(self, exc):
-        super().connection_lost(exc)
 
-        if not self._waiter.done():
-            self._waiter.set_result(None)
+class ASGIWrapper:
+    def __init__(self, app, scope, request, response):
+        self.app = app
+        self.scope = scope
+        self.request = request
+        self.response = response
+        self.loop = request.protocol.loop
+        self.logger = request.protocol.logger
+        self._websocket = None
 
-        self.response = None
-        self._scope.clear()
+        if self.scope['type'] == 'websocket':
+            self._websocket = WebSocket(request, response)
+        else:
+            self._stream = request.stream()
+
+    def __await__(self):
+        return self.app(self.scope, self.receive, self.send).__await__()
+
+    @property
+    def protocol(self):  # don't cache request.protocol
+        return self.request.protocol
 
     async def receive(self):
-        if self._scope['type'] == 'websocket':
+        if self.scope['type'] == 'websocket':
             # initially, the Request.upgraded value is False
             # it will become True later
             # after the response status is set to 101:
@@ -136,30 +139,30 @@ class ASGIServer(HTTPProtocol):
                         'calling receive() after the connection is closed'
                     )
                 else:
-                    self.print_exception(exc)
+                    self.protocol.print_exception(exc)
 
-                self.set_handler_timeout(self.options['app_close_timeout'])
+                    self.protocol.request = None  # force handler timeout
+                    self.protocol.set_handler_timeout(
+                        self.protocol.options['app_close_timeout']
+                    )
                 return {
                     'type': 'websocket.disconnect',
                     'code': code
                 }
 
-        if self._scope['type'] != 'http':
+        if self.scope['type'] != 'http':
             await self.response.handle_exception(
                 InternalServerError('unsupported scope type %s' %
-                                    self._scope['type'])
+                                    self.scope['type'])
             )
             return
 
         try:
-            data = await self._read.__anext__()
-            more_body = (
-                (data != b'' and self.request.content_length == -1) or
-                self.request.body_consumed < self.request.content_length
-            )
+            data = await self._stream.__anext__()
+            more_body = self.request.has_body and not self.request.eof()
 
             if not more_body:
-                self._read = None
+                self._stream = None
 
             return {
                 'type': 'http.request',
@@ -167,28 +170,31 @@ class ASGIServer(HTTPProtocol):
                 'more_body': more_body
             }
         except (asyncio.CancelledError, Exception) as exc:
-            if self.request is None:
+            if self.protocol is None or self.protocol.is_closing():
                 self.logger.info(
                     'calling receive() after the connection is closed'
                 )
-            elif self._read is None or isinstance(exc, StopAsyncIteration):
-                # delay http.disconnect (a workaround for Quart)
-                # https://github.com/nggit/tremolo/issues/202
-                if self._waiter.done():
-                    self._waiter = self.loop.create_future()
-
-                if self._read is not None:
-                    self._read = None
-                    return {'type': 'http.request'}
-
-                self.logger.info(
-                    'calling receive() when there is no more body'
-                )
-                await self._waiter
             else:
-                self.print_exception(exc)
+                if self._stream is None or isinstance(exc, StopAsyncIteration):
+                    # delay http.disconnect (a workaround for Quart)
+                    # https://github.com/nggit/tremolo/issues/202
+                    if self.protocol.ON_CLOSE.done():
+                        self.protocol.ON_CLOSE = self.loop.create_future()
 
-            self.set_handler_timeout(self.options['app_close_timeout'])
+                    if self._stream is not None:
+                        self._stream = None
+                        return {'type': 'http.request'}
+
+                    self.logger.info(
+                        'calling receive() when there is no more body'
+                    )
+                    await self.protocol.ON_CLOSE
+                else:
+                    self.protocol.print_exception(exc)
+
+                self.protocol.set_handler_timeout(
+                    self.protocol.options['app_close_timeout']
+                )
             return {'type': 'http.disconnect'}
 
     async def send(self, data):
@@ -252,15 +258,15 @@ class ASGIServer(HTTPProtocol):
                 if 'body' in data and data['body'] != b'':
                     await self.response.write(
                         data['body'],
-                        rate=self.options['download_rate'],
-                        buffer_size=self.options['buffer_size']
+                        rate=self.protocol.options['download_rate'],
+                        buffer_size=self.protocol.options['buffer_size']
                     )
 
                 if 'more_body' not in data or data['more_body'] is False:
                     await self.response.write(b'')
                     self.response.close(keepalive=True)
-                    self._read = None  # disallows further receive()
-                    self._waiter.cancel()
+                    self._stream = None  # disallows further receive()
+                    self.protocol.ON_CLOSE.cancel()
             elif data['type'] == 'websocket.send':
                 if 'bytes' in data and data['bytes']:
                     await self._websocket.send(data['bytes'])
@@ -271,13 +277,13 @@ class ASGIServer(HTTPProtocol):
             elif data['type'] == 'websocket.close':
                 await self._websocket.close(data.get('code', 1000))
                 self._websocket = None
-                self._waiter.cancel()
+                self.protocol.ON_CLOSE.cancel()
         except asyncio.CancelledError:
             pass
         except Exception as exc:
-            if self.request is None or self.response is None:
+            if self.protocol is None or self.protocol.is_closing():
                 self.logger.info(
                     'calling send() after the connection is closed'
                 )
             else:
-                self.print_exception(exc)
+                self.protocol.print_exception(exc)
