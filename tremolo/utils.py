346510042961c8155c832a6e0abcb5be86227731
nggit <contact@anggit.com>
2025-02-26 07:15:01 +0700

    clean up utils, add parse_int()

--- tremolo/utils.py
+++ tremolo/utils.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2023 nggit
 
-__all__ = ('file_signature', 'html_escape', 'log_date', 'memory_usage',
-           'server_date', 'getoptions', 'parse_fields', 'parse_args')
+__all__ = ('file_signature', 'getoptions', 'html_escape', 'log_date',
+           'memory_usage', 'parse_args', 'parse_fields', 'parse_int',
+           'server_date')
 
 import os  # noqa: E402
 import stat  # noqa: E402
@@ -16,6 +17,23 @@ def file_signature(path):
     st = os.stat(path)
 
     return (stat.S_IFMT(st.st_mode), st.st_size, st.st_mtime)
+
+
+def getoptions(func):
+    options = {}
+    argcount = func.__code__.co_argcount
+
+    if func.__defaults__ is not None:
+        argcount -= len(func.__defaults__)
+
+    for name in func.__code__.co_varnames[:argcount]:
+        options[name] = None
+
+    for i, name in enumerate(func.__code__.co_varnames[
+                                 argcount:func.__code__.co_argcount]):
+        options[name] = func.__defaults__[i]
+
+    return options
 
 
 def html_escape(data):
@@ -42,47 +60,6 @@ def memory_usage(pid=0):
     except FileNotFoundError:
         # non-Linux
         return -1
-
-
-def server_date():
-    return datetime.now(timezone.utc).strftime(
-        '%a, %d %b %Y %H:%M:%S GMT').encode('latin-1')
-
-
-def getoptions(func):
-    options = {}
-    argcount = func.__code__.co_argcount
-
-    if func.__defaults__ is not None:
-        argcount -= len(func.__defaults__)
-
-    for name in func.__code__.co_varnames[:argcount]:
-        options[name] = None
-
-    for i, name in enumerate(func.__code__.co_varnames[
-                                 argcount:func.__code__.co_argcount]):
-        options[name] = func.__defaults__[i]
-
-    return options
-
-
-def parse_fields(data, separator=b';', max_fields=100):
-    if len(separator) != 1:
-        raise ValueError('separator must be a single one-byte character')
-
-    end = len(data)
-
-    for _ in range(max_fields):
-        start = data.rfind(separator, 0, end) + 1
-        name, _, value = data[start:end].partition(b'=')
-
-        if name:
-            yield (name.strip().lower(), unquote(value.strip(b' \t"')))
-
-        if start == 0:
-            break
-
-        end = start - 1
 
 
 def parse_args(**callbacks):
@@ -143,3 +120,39 @@ def parse_args(**callbacks):
                 sys.exit(1)
 
     return options
+
+
+def parse_fields(data, separator=b';', max_fields=100):
+    if len(separator) != 1:
+        raise ValueError('separator must be a single one-byte character')
+
+    end = len(data)
+
+    for _ in range(max_fields):
+        start = data.rfind(separator, 0, end) + 1
+        name, _, value = data[start:end].partition(b'=')
+
+        if name:
+            yield (name.strip().lower(), unquote(value.strip(b' \t"')))
+
+        if start == 0:
+            break
+
+        end = start - 1
+
+
+def parse_int(string, base=10):
+    if not isinstance(string, (bytes, bytearray)) or len(string) > 16:
+        raise ValueError('string must be bytes-like and <=16 in length')
+
+    total = 0
+
+    for c in string:
+        total = total * base + int(chr(c), base)
+
+    return total
+
+
+def server_date():
+    return datetime.now(timezone.utc).strftime(
+        '%a, %d %b %Y %H:%M:%S GMT').encode('latin-1')
