4d2cb19ed34a7e07b42473cdf3ddcd314a44f89c
Zen <zen@credenso.cafe>
2025-05-19 08:59:22 -0400

    session-keyed notifications

--- examples/sse_notifications.py
+++ examples/sse_notifications.py
@@ -1,11 +1,21 @@
 import asyncio
 import json
 from tremolo import Application
+from uuid import uuid4
 
 app = Application()
 
+listeners = {}
+
 @app.route('/')
-async def index(**server):
+async def index(request, response, **server):
+    try:
+        [s] = request.cookies.get('session')
+        if s not in listeners: raise TypeError
+    except TypeError:
+        s = str(uuid4())
+        response.set_cookie('session', s, expires=3600)
+
     html = '''
     <body>
         <p>The password is 'tremolo'</p>
@@ -14,6 +24,8 @@ async def index(**server):
             <input name="password" type="password" />
             <button>Login</button>
         </form>
+        <button onclick='fetch("/ping")'>Ping Everyone</button>
+        <div class="notifications"></div>
         <script>
         const form = document.querySelector('#form') 
         form.addEventListener('submit', (e) => {
@@ -21,13 +33,15 @@ async def index(**server):
             const data = {
                 password: form.querySelector('input').value
             }
+
+            const form_data = new FormData(form)
             form.reset()
-            fetch('/login', {
+            const res = fetch('/login', {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
-                body: JSON.stringify(data)
+                body: new URLSearchParams(form_data)
             })
         })
 
@@ -38,7 +52,8 @@ async def index(**server):
             n.innerHTML = `${data.text}`
             n.classList.add('notification')
             n.classList.add(data.class)
-            document.body.insertBefore(n, form)
+            const box = document.querySelector('.notifications')
+            box.appendChild(n)
 
             setTimeout(() => n.classList.add('faded'), 1500)
             setTimeout(() => n.remove(), 2000)
@@ -46,21 +61,26 @@ async def index(**server):
         })
         </script>
         <style>
-        .notification {
+        .notifications {
             position: absolute;
+            display: flex;
+            flex-direction: column;
+            gap: 1em;
             bottom: 1em;
             right: 1em;
+        }
+
+        .notifications div {
             color: white;
             padding: 0.5em;
-            background: green;
             box-shadow: 0.1em 0.1em 0.5em grey;
         }
 
-        .notification.message {
+        .notifications div.message {
             background: green;
         }
 
-        .notification.error {
+        .notifications div.error {
             background: red;
         }
 
@@ -84,42 +104,56 @@ async def index(**server):
 
     yield html.encode()
 
-listeners = set()
-
 @app.route('/notifications$')
-async def notify(sse=None, **server):
+async def notify(request, sse=None, **server):
+    try:
+        [s] = request.cookies.get('session')
+    except TypeError:
+        return None
+
     if sse:
         msg = { 'text': 'listening', 'class': 'message' }
         await sse.send(json.dumps(msg).encode(), event='notify')
 
         queue = asyncio.Queue(maxsize=1)
-        listeners.add(queue)
+        listeners[s] = queue
 
         try:
             while True:
                 ev, data = await queue.get()
                 await sse.send(json.dumps(data).encode(), event=ev)
         finally:  # the client has gone, clean up
-            listeners.discard(queue)
+            listeners.pop(s, None)
 
 
 @app.route('login$')
 async def login(request):
-    data = await request.body()
-    data = json.loads(data.decode())
-
-    # Not retrieving form data correctly.
-    password = data.get('password') or "not working"
+    data = await request.form()
+    [password] = data.get('password')
 
     if password == "tremolo":
         notification = {'text': 'correct password', 'class': 'message'}
     else:
         notification = {'text': 'incorrect password', 'class': 'error'}
 
-    for queue in listeners:
+    try:
+        [s] = request.cookies.get('session')
+        queue = listeners[s]
+        queue.put_nowait(['notify', notification])
+        yield b'login'
+
+    except TypeError:
+        yield None
+
+
+@app.route('ping$')
+async def ping(request):
+    notification = {'text': 'ping!', 'class': 'message'}
+    for queue in listeners.values():
         queue.put_nowait(['notify', notification])
 
     return f'ping to {len(listeners)} listeners'
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 8001, debug=True)
