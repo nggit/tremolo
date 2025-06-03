dd4d93970c18dd88d4ce8ded597bffbc5fce5a88
nggit <contact@anggit.com>
2025-06-03 11:18:43 +0700

    add executors.py

---
+++ tremolo/lib/executors.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2023 nggit
+
+import asyncio
+import queue
+
+from functools import wraps
+from inspect import isgeneratorfunction
+from threading import Thread
+
+
+def set_result(fut, result):
+    if not fut.done():
+        fut.set_result(result)
+
+
+def set_exception(fut, exc):
+    if not fut.done():
+        fut.set_exception(exc)
+
+
+class ThreadExecutor(Thread):
+    def __init__(self, context, loop=None, **kwargs):
+        super().__init__(**kwargs)
+
+        self.context = context
+        self.queue = queue.SimpleQueue()
+        self.loop = loop or asyncio.get_event_loop()
+        self._shutdown = None
+
+    def run(self):
+        while True:
+            try:
+                self.context.thread = self  # update the last active thread
+                fut, func, args, kwargs = self.queue.get(timeout=1)
+            except queue.Empty:
+                continue
+
+            if func is None:
+                self.loop.call_soon_threadsafe(self.join)
+                self.loop.call_soon_threadsafe(set_result, fut, None)
+                break
+
+            try:
+                self.loop.call_soon_threadsafe(
+                    set_result, fut, func(*args, **kwargs)
+                )
+            except StopIteration:
+                # StopIteration interacts badly with generators
+                # and cannot be raised into a Future
+                self.loop.call_soon_threadsafe(fut.cancel)
+            except BaseException as exc:
+                self.loop.call_soon_threadsafe(set_exception, fut, exc)
+
+    def submit(self, func, *args, **kwargs):
+        if not self.is_alive():
+            raise RuntimeError(
+                'calling submit() before start() or after shutdown()'
+            )
+
+        if isgeneratorfunction(func):
+            gen = func(*args, **kwargs)
+
+            @wraps(func)
+            async def wrapper():
+                while True:
+                    fut = self.loop.create_future()
+                    self.queue.put_nowait((fut, gen.__next__, (), {}))
+
+                    try:
+                        yield await fut
+                    except asyncio.CancelledError:
+                        break
+
+            return wrapper()
+
+        if callable(func):
+            fut = self.loop.create_future()
+            self.queue.put_nowait((fut, func, args, kwargs))
+
+            return fut
+
+        raise TypeError(f'{str(func)} is not generator function or callable')
+
+    def shutdown(self):
+        if self._shutdown is None:
+            self._shutdown = self.loop.create_future()
+
+            if self.is_alive():
+                self.queue.put_nowait((self._shutdown, None, None, None))
+
+        if not self.is_alive():
+            set_result(self._shutdown, None)
+
+        return self._shutdown
+
+
+class MultiThreadExecutor:
+    def __init__(self, size=5):
+        self.size = size
+        self.threads = []
+        self.thread = None  # points to the last active thread
+
+    def start(self, prefix='MultiThreadExecutor', **kwargs):
+        while len(self.threads) < self.size:
+            self.thread = ThreadExecutor(
+                self, name=f'{prefix}-{len(self.threads)}', **kwargs
+            )
+            self.thread.start()
+            self.threads.append(self.thread)
+
+    def submit(self, func, args=(), kwargs={}, name=None):
+        if self.size == 0 or len(self.threads) != self.size:
+            raise RuntimeError('no running threads')
+
+        if name is None:
+            return self.thread.submit(func, *args, **kwargs)
+
+        return self.threads[name % self.size].submit(func, *args, **kwargs)
+
+    async def shutdown(self):
+        self.size = 0
+
+        while self.threads:
+            await self.threads.pop().shutdown()
