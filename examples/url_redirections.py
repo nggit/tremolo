b7964c4d003ea012a758c491aa31b65a64e8fca2
nggit <contact@anggit.com>
2025-07-30 11:56:09 +0700

    add url_redirections example

---
+++ examples/url_redirections.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+# Author: nggit
+# Description: URL Redirections
+
+from tremolo import Application
+
+app = Application()
+
+
+@app.route('/example')
+async def redirect_handler(response, **server):
+    # redirection is basically just to set the header and return an empty body
+    response.set_status(301, 'Moved Permanently')
+    response.set_header('Location', 'http://example.com/')
+    return ''
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8000)
