811cad3a7aa876dcf63f379d54f3054c8daa2872
nggit <contact@anggit.com>
2025-11-17 07:12:04 +0700

    split test_http_malformed

--- tests/test_http.py
+++ tests/test_http.py
@@ -102,20 +102,6 @@ class TestHTTP(unittest.TestCase):
                 response.body(), b'GET /getheaderline?foo HTTP/1.1'
             )
 
-    def test_get_doublehost_11(self):
-        with self.client:
-            self.client.sendall(
-                b'GET /gethost HTTP/1.1\r\n'
-                b'Host: localhost\r\n'
-                b'Host: host.local\r\n\r\n'
-            )
-
-            response = self.client.end()
-
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-            self.assertEqual(response.body(), b'Bad Request')
-
     def test_get_query_11(self):
         with self.client:
             response = self.client.send(
@@ -286,66 +272,6 @@ class TestHTTP(unittest.TestCase):
                 response.headers[b'content-type'],
                 [b'application/octet-stream']
             )
-
-    def test_post_bad_chunked_encoding(self):
-        with self.client2:
-            response = self.client2.send(
-                b'POST /upload HTTP/1.1',
-                b'Transfer-Encoding: chunked'
-            )
-
-            self.client2.sendall(b'-1\r\n')
-
-            self.assertEqual(response.body(), b'bad chunked encoding')
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-
-    def test_post_no_chunk_size(self):
-        with self.client2:
-            response = self.client2.send(
-                b'POST /upload HTTP/1.1',
-                b'Transfer-Encoding: chunked'
-            )
-
-            self.client2.sendall(b'X' * 65)
-
-            self.assertEqual(
-                response.body(), b'bad chunked encoding: no chunk size'
-            )
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-
-    def test_post_invalid_chunk_terminator(self):
-        with self.client2:
-            response = self.client2.send(
-                b'POST /upload HTTP/1.1',
-                b'Transfer-Encoding: chunked'
-            )
-
-            self.client2.sendall(b'1\r\nA\rX')
-
-            self.assertEqual(
-                response.body(),
-                b'bad chunked encoding: invalid chunk terminator'
-            )
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-
-    def test_post_invalid_chunk_end(self):
-        with self.client2:
-            response = self.client2.send(
-                b'POST /upload HTTP/1.1',
-                b'Transfer-Encoding: chunked'
-            )
-
-            self.client2.sendall(b'0;\r\n\rX')
-
-            self.assertEqual(
-                response.body(),
-                b'bad chunked encoding: invalid chunk terminator'
-            )
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
 
     def test_post_upload_maxqueue(self):
         with self.client2:
@@ -555,82 +481,6 @@ class TestHTTP(unittest.TestCase):
             self.assertEqual(response.header.version, b'HTTP/1.1')
             self.assertEqual(response.status, 404)
             self.assertEqual(response.message, b'Not Found')
-
-    def test_get_badrequest(self):
-        with self.client:
-            response = self.client.send(b'GET HTTP/')
-
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-
-    def test_badrequest_notarequest(self):
-        with self.client:
-            response = self.client.send(b' HTTP/')
-
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-            self.assertEqual(response.body(), b'bad request: not a request')
-
-    def test_badrequest(self):
-        with self.client:
-            self.client.sendall(b'GET / HTTP/1.1\r\nHost: localhost\r\n')
-            self.client.sendall(b'\x00' * 8192)
-
-            response = self.client.end()
-
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-            self.assertEqual(response.body(), b'bad request')
-
-    def test_headertoolarge(self):
-        with self.client:
-            response = self.client.send(
-                b'GET / HTTP/1.1',
-                b'Host: ' + b'\x00' * 8192
-            )
-
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-            self.assertEqual(response.body(), b'request header too large')
-
-    def test_sec_content_length_and_transfer_encoding(self):
-        with self.client:
-            response = self.client.send(
-                b'GET /upload HTTP/1.1',
-                b'Content-Length: 5',
-                b'Transfer-Encoding: chunked'
-            )
-
-            self.client.sendall(b'0\r\n\r\n')
-
-            self.assertEqual(response.body(), b'ambiguous Content-Length')
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-
-    def test_sec_double_content_length(self):
-        with self.client:
-            response = self.client.send(
-                b'GET /upload HTTP/1.1',
-                b'Content-Length: 1',
-                b'Content-Length: 2'
-            )
-
-            self.client.sendall(b'AB')
-
-            self.assertEqual(response.body(), b'ambiguous Content-Length')
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
-
-    def test_sec_empty_content_length(self):
-        with self.client:
-            response = self.client.send(
-                b'GET /upload HTTP/1.1',
-                b'Content-Length: '
-            )
-
-            self.assertEqual(response.body(), b'bad Content-Length')
-            self.assertEqual(response.status, 400)
-            self.assertEqual(response.message, b'Bad Request')
 
     def test_requesttimeout(self):
         with self.client1:
