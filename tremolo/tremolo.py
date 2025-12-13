5f18a7fa3f0be6c2382c1276044a0724d8e7d841
nggit <contact@anggit.com>
2025-12-13 10:46:29 +0700

    release 0.4.19

--- tremolo/tremolo.py
+++ tremolo/tremolo.py
@@ -25,7 +25,7 @@ from .lib.contexts import WorkerContext
 from .lib.executors import MultiThreadExecutor
 from .lib.locks import ServerLock
 
-__version__ = '0.4.18'
+__version__ = '0.4.19'
 
 
 class Tremolo:
